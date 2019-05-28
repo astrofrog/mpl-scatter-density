d9bbce2322b7ece3503523b5a2bd97a06737f581
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-05-28 16:58:06 +0100

    Updated tests to make them more robust to Qt or Tk not being installed

--- mpl_scatter_density/tests/test_base_image_artist.py
+++ mpl_scatter_density/tests/test_base_image_artist.py
@@ -1,7 +1,14 @@
+import pytest
+
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg
-from matplotlib.backends.backend_tkagg import FigureCanvasTk
-from matplotlib.backends.backend_qt5 import FigureCanvasQT
+
+try:
+    from matplotlib.backends.backend_qt5 import FigureCanvasQT
+except ImportError:
+    QT_INSTALLED = False
+else:
+    QT_INSTALLED = True
 
 from ..base_image_artist import supports_resize
 
@@ -11,16 +18,17 @@ def test_supports_resize():
     canvas = FigureCanvasAgg(Figure())
     assert not supports_resize(canvas)
 
-    canvas = FigureCanvasTk(Figure())
-    assert supports_resize(canvas)
-
-    canvas = FigureCanvasQT(Figure())
-    assert supports_resize(canvas)
-
     class SubclassA(FigureCanvasAgg):
         pass
     canvas = SubclassA(Figure())
     assert not supports_resize(canvas)
+
+
+@pytest.mark.skipif('not QT_INSTALLED')
+def test_supports_resize_qt():
+
+    canvas = FigureCanvasQT(Figure())
+    assert supports_resize(canvas)
 
     class SubclassB(FigureCanvasQT):
         pass
