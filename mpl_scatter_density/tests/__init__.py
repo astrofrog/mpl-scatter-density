e0807f4437bcfa4031c94912a9c097cf66886456
Thomas Robitaille <thomas.robitaille@gmail.com>
2023-07-04 11:11:41 +0100

    Remove setup.py file and no longer check for Matplotlib version for image tests

--- mpl_scatter_density/tests/__init__.py
+++ mpl_scatter_density/tests/__init__.py
@@ -1,16 +1,4 @@
-import os
-
-from packaging.version import Version
-
-import matplotlib
-
-MPL_VERSION = Version(matplotlib.__version__)
-
 baseline_root = 'baseline'
-
-if MPL_VERSION >= Version('3'):  # pragma: nocover
-    baseline_subdir = '3.0.x'
-else:  # pragma: nocover
-    raise ValueError('Matplotlib {0} is not supported'.format(matplotlib.__version__))
+baseline_subdir = '3.0.x'
 
 baseline_dir = os.path.join(baseline_root, baseline_subdir)
