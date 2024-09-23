3c238ecca635d48d7c45c4329a3a5e273ea0797c
Clément Robert <cr52@protonmail.com>
2024-09-23 13:28:50 +0200

    DEP: drop support for CPython 3.6 to 3.8

--- mpl_scatter_density/__init__.py
+++ mpl_scatter_density/__init__.py
@@ -1,11 +1,8 @@
 from .scatter_density_artist import *  # noqa
 from .scatter_density_axes import *  # noqa
 
-import sys
-if sys.version_info >= (3, 8):
-    from importlib.metadata import version
-else:
-    from importlib_metadata import version
+
+from importlib.metadata import version
 
 __version__ = version("mpl-scatter-density")
-del version, sys
+del version
